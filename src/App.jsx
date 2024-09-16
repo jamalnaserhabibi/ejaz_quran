08e80742eb7bff4ce81da3adac2f2090df023071
jamalnaserhabibi <jamalnaser.tech@gmail.com>
2024-09-16 08:37:02 +0430

    done

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import NavBar from './components/NavBar/NavBar'
+import Home from './components/Home/Home'
+import './App.css'
+export default function App() {
+
+  return (
+    <>
+     <NavBar />
+     <Home />
+    </>
+  )
+}
