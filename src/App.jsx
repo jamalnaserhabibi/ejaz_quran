fd17895a72b5e53dc703f47dc735850998edb070
jamalnaserhabibi <jamalnaser.tech@gmail.com>
2024-10-16 14:58:14 +0430

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-import NavBar from './components/NavBar/NavBar'
-import Home from './components/Home/Home'
-import './App.css'
-
-import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from "./components/NavBar/NavBar";
+import FirstLoad from "./components/firstload";
+import Quran from "./components/Quran/Quran";
+import "./App.css";
+import { BrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 export default function App() {
-
   return (
     <>
-     <NavBar />
-     <Home />
-     {/* <Home /> */}
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<FirstLoad />} />
+          <Route path="/quran" element={<Quran />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
+import { Link } from "react-router-dom";
 import Navbar from "react-bootstrap/Navbar";
 import { Divide as Hamburger } from 'hamburger-react';
 import "./NavBar.css";
-
+import ScrollToTop from "react-scroll-to-top";
 export default function NavBar() {
   const [isOpen, setIsOpen] = useState(false);
 
@@ -12,24 +13,26 @@ export default function NavBar() {
     <div className="nav_main">
       <Navbar expanded={isOpen} expand="lg" className="navbar">
         <Container>
-          <Navbar.Brand  className="navLogo" href="#home">
+          <Navbar.Brand className="navLogo" href="#home">
             اعجاز قرآن
           </Navbar.Brand>
           <Hamburger toggled={isOpen} toggle={setIsOpen} />
           <Navbar.Collapse in={isOpen} id="basic-navbar-nav">
-            <Nav  className="navButton ms-auto">
-            <span style={{height:"20px"}} className="space"></span>
+            <Nav className="navButton ms-auto">
+              <span style={{ height: "20px" }} className="space"></span>
               <Nav.Link href="#home">خانه</Nav.Link>
               <Nav.Link href="#lesson">تقرير ها</Nav.Link>
-              <Nav.Link href="#quran">قرآنشريف</Nav.Link>
+              <Nav.Link as={Link} to="/quran">قرآنشريف</Nav.Link>
               <Nav.Link href="#source">کتاب ها</Nav.Link>
               <Nav.Link href="#about">در باره ما</Nav.Link>
               <Nav.Link href="#contact">ارتباطات</Nav.Link>
-              <span style={{height:"80px"}} className="space"></span>
+              <span style={{ height: "80px" }} className="space"></span>
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
+      <ScrollToTop smooth />
     </div>
+    
   );
 }
